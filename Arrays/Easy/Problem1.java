2b523da7f18fe62687228b1718000e8bb7f902a1
siddyamgond <117370082+siddyamgond@users.noreply.github.com>
2024-03-30 20:40:27 +0530

    Largest number from array 2

--- Arrays/Easy/Problem1.java
+++ Arrays/Easy/Problem1.java
@@ -1,4 +1,5 @@
 // Find Largest number 
+// Solution 1
 import java.util.* ;
 import java.io.*; 
 
@@ -13,5 +14,19 @@ public class Solution {
             }
         }
         return max;
+    }
+}
+
+
+// Solution 2
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+    static int largestElement(int[] arr, int n) {
+        // Write your code here.
+        Arrays.sort(arr);
+        return arr[n-1];
     }
 }
