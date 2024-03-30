356fdb7737805f123452f029113abbfd4ce18fe8
siddyamgond <117370082+siddyamgond@users.noreply.github.com>
2024-03-30 22:17:05 +0530

    Create Pattern2.java

---
+++ Pattern solving/Pattern2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      for(int i=0;i<5;i++){
+        for(int j=0;j<i+1;j++){
+          System.out.print(" * ");
+        }
+        System.out.println("");
+      }
+  }
+}
